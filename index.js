b9bd34c2bba3c3483e63124e51072a55b557d666
JW <daxon2012@gmail.com>
2023-09-29 14:25:16 -0700

    successful adding question

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the project title?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Descriptions"
+    },
+    {
+        type: "input",
+        name: "installationInstruction",
+        message: "Instructions"
+    },
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers)=>{})
+ }
 
 // Function call to initialize app
 init();
