20f6500ea6149740885261d66ada24250af7fec5
JW <daxon2012@gmail.com>
2023-10-10 20:58:56 -0700

    Added renderListLink function

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,29 +1,57 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  // I am using the switch statement to handle the user's selected cases
-  switch (license) {
-    case 'Apache':
-      return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+  // The if statement make sure that the no license scenario is handle
+  if (license) {
+    // I am using the switch statement to handle the user's selected cases
+    switch (license) {
+      case 'Apache':
+        return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
 
-    case 'GPLv3':
-      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+      case 'GPLv3':
+        return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
 
-    case 'IPL':
-      return '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)'
+      case 'IPL':
+        return '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)'
 
-    case 'MIT':
-      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+      case 'MIT':
+        return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
 
-    default:
-      return ""
+      default:
+        return "";
+    }
+  } else {
+      return "";
   }
-
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+   // The if statement make sure that the no license scenario is handle
+   if (license) {
+    // I am using the switch statement to handle the user's selected cases
+    switch (license) {
+      case 'Apache':
+        return 'https://opensource.org/licenses/Apache-2.0'
+
+      case 'GPLv3':
+        return 'https://www.gnu.org/licenses/gpl-3.0'
+
+      case 'IPL':
+        return 'https://opensource.org/licenses/IPL-1.0'
+
+      case 'MIT':
+        return 'https://opensource.org/licenses/MIT'
+
+      default:
+        return "";
+    }
+  } else {
+      return "";
+  }
+}
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
